03dfa21c9ceb922225cf6fd6ef8ddf4ae32089cf
Guilherme Bittencourt <guilherme.assis.bittencourt@gmail.com>
2025-10-03 16:24:50 -0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    } else {
+      entry.target.classList.remove('visible');
+    }
+  });
+}, {
+  threshold: 0
+});
+
+const sections = document.querySelectorAll('section');
+
+sections.forEach(section => {
+  observer.observe(section);
+});
